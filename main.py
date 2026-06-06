48c177541da9b5aee668b04f2288f7ff56b11510
vadaay <69793395+vadaay@users.noreply.github.com>
2026-06-06 23:34:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def signal(change):
 
 def send_post():
     token = os.environ["8807568809:AAGRLm0KbFGR6pJqwIQABcwwHNahlK37Rro"]
-    channel = "@crypto-x-news"
+    channel = "@finanis"
 
     data = get_crypto()
 
